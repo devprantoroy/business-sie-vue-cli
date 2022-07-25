fb9149dbd8ad0839099f455aa02fbcf847d9c518
Pranto Roy <www.prantoroy.com@gmail.com>
2022-07-25 12:40:41 +0600

    Backend auth start

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}"/>
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -21,6 +21,20 @@
         </style>
     </head>
     <body class="antialiased">
+    @if (Auth::check())
+        <script>
+            window.Laravel = {!!json_encode([
+                'isLoggedin' => true,
+                'user' => Auth::user()
+            ])!!}
+        </script>
+    @else
+        <script>
+            window.Laravel = {!!json_encode([
+                'isLoggedin' => false
+            ])!!}
+        </script>
+    @endif
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
